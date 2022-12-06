7921cd60179e0ae322f5607567831d2474dde37b
Никита Бачу <fair.nik@yandex.ru>
2022-12-06 21:07:11 +0400

    xd

--- yatube/posts/tests/test_routes.py
+++ yatube/posts/tests/test_routes.py
@@ -2,33 +2,38 @@ from django.test import TestCase
 from django.urls import reverse
 
 # Не понимаю почему это строка лишняя,
-#  если мы ее удалим, то как я создам в дб объекты,
-#   чтоб сделать к ним запросы
+#  если мы ее удалим, то как я создам в дб объекты
 from ..models import Group, Post, User
 
 
-author_user = User.objects.create(username='Author')
-post = Post.objects.create(
-    text='Тестовый текст',
-    author=author_user)
-
-group = Group.objects.create(
-    title='Тестовый заголовок',
-    description='Тестовое описание',
-    slug='group-slug')
-
-PATHS = {
-    reverse('posts:index'): '/',
-    reverse('posts:post_create'): '/create/',
-    reverse('posts:post_edit', args=[post.id]): '/posts/1/edit/',
-    reverse('posts:group_posts', args=[group.slug]): '/group/group-slug/',
-    reverse('posts:profile', args=[author_user.username]): '/profile/Author/',
-    reverse('posts:post_detail', args=[post.id]): '/posts/1/'
-
-}
-
-
 class RoutesTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.author = User.objects.create(username='Author')
+        cls.group = Group.objects.create(
+            title='Тестовый заголовок',
+            description='Тестовое описание',
+            slug='group-slug'
+        )
+        cls.post = Post.objects.create(
+            text='Тестовый текст',
+            author=RoutesTests.author,
+            group=RoutesTests.group
+        )
+        cls.PATHS = {
+            reverse('posts:index'): '/',
+            reverse('posts:post_create'): '/create/',
+            reverse('posts:post_edit', args=[RoutesTests.post.id]):
+                '/posts/1/edit/',
+            reverse('posts:group_posts', args=[RoutesTests.group.slug]):
+                '/group/group-slug/',
+            reverse('posts:profile', args=[RoutesTests.author.username]):
+                '/profile/Author/',
+            reverse('posts:post_detail', args=[RoutesTests.post.id]):
+                '/posts/1/'
+        }
+
     def test__routes(self):
-        for reversed_path, path in PATHS:
+        for reversed_path, path in self.PATHS.items():
             self.assertEqual(reversed_path, path)
